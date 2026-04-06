cb1338833a357fc8e3e098f02bce44a93a7fbfcd
Max <z1305712215@gmail.com>
2026-04-06 14:57:35 -0600

    Uploaded the initial SpritePreviewer and images

---
+++ SpritePreviewer.py
@@ -0,0 +1,59 @@
+import math
+
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+
+# This function loads a series of sprite images stored in a folder with a
+# consistent naming pattern: sprite_# or sprite_##. It returns a list of the images.
+def load_sprite(sprite_folder_name, number_of_frames):
+    frames = []
+    padding = math.ceil(math.log(number_of_frames - 1, 10))
+    for frame in range(number_of_frames):
+        folder_and_file_name = sprite_folder_name + "/sprite_" + str(frame).rjust(padding, '0') + ".png"
+        frames.append(QPixmap(folder_and_file_name))
+
+    return frames
+
+class SpritePreview(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Sprite Animation Preview")
+        # This loads the provided sprite and would need to be changed for your own.
+        self.num_frames = 21
+        self.frames = load_sprite('spriteImages',self.num_frames)
+
+        # Add any other instance variables needed to track information as the program
+        # runs here
+
+        # Make the GUI in the setupUI method
+        self.setupUI()
+
+
+    def setupUI(self):
+        # An application needs a central widget - often a QFrame
+        frame = QFrame()
+
+        # Add a lot of code here to make layouts, more QFrame or QWidgets, and
+        # the other components of the program.
+        # Create needed connections between the UI components and slot methods
+        # you define in this class.
+
+        self.setCentralWidget(frame)
+
+
+    # You will need methods in the class to act as slots to connect to signals
+
+
+def main():
+    app = QApplication([])
+    # Create our custom application
+    window = SpritePreview()
+    # And show it
+    window.show()
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
